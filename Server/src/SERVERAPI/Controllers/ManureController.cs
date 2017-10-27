7fc5e142b2f7d87663de1d53fd044f226c245745
tntbc <tnt_bc@hotmail.com>
2017-10-27 09:20:20 -0700

    recalc bal when manure updated

--- Server/src/SERVERAPI/Controllers/ManureController.cs
+++ Server/src/SERVERAPI/Controllers/ManureController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using SERVERAPI.ViewModels;
 using SERVERAPI.Models.Impl;
 using SERVERAPI.Models;
+using static SERVERAPI.Models.StaticData;
+using SERVERAPI.Utility;
 
 namespace SERVERAPI.Controllers
 {
@@ -375,6 +377,8 @@ namespace SERVERAPI.Controllers
                         }
 
                         _ud.UpdateFarmManure(fm);
+
+                        ReCalculateManure(fm.id);
                     }
 
                     string url = Url.Action("RefreshCompostList", "Manure");
@@ -387,6 +391,52 @@ namespace SERVERAPI.Controllers
             }
 
             return PartialView(cvm);
+        }
+        private void ReCalculateManure(int id)
+        {
+            Utility.CalculateNutrients calculateNutrients = new CalculateNutrients(_env, _ud, _sd);
+            NOrganicMineralizations nOrganicMineralizations = new NOrganicMineralizations();
+
+            List<Field> flds = _ud.GetFields();
+
+            foreach (var fld in flds)
+            {
+                List<NutrientManure> mans = _ud.GetFieldNutrientsManures(fld.fieldName);
+
+                foreach (var nm in mans)
+                {
+                    if (id.ToString() == nm.manureId)
+                    {
+                        int regionid = _ud.FarmDetails().farmRegion.Value;
+                        Region region = _sd.GetRegion(regionid);
+                        nOrganicMineralizations = calculateNutrients.GetNMineralization(Convert.ToInt16(nm.manureId), region.locationid);
+
+                        string avail = (nOrganicMineralizations.OrganicN_FirstYear * 100).ToString("###");
+
+                        string nh4 = (calculateNutrients.GetAmmoniaRetention(Convert.ToInt16(nm.manureId), Convert.ToInt16(nm.applicationId)) * 100).ToString("###");
+
+                        NutrientInputs nutrientInputs = new NutrientInputs();
+
+                        calculateNutrients.manure = nm.manureId;
+                        calculateNutrients.applicationSeason = nm.applicationId;
+                        calculateNutrients.applicationRate = Convert.ToDecimal(nm.rate);
+                        calculateNutrients.applicationRateUnits = nm.unitId;
+                        calculateNutrients.ammoniaNRetentionPct = Convert.ToDecimal(nh4);
+                        calculateNutrients.firstYearOrganicNAvailablityPct = Convert.ToDecimal(avail);
+
+                        calculateNutrients.GetNutrientInputs(nutrientInputs);
+
+                        nm.yrN = nutrientInputs.N_FirstYear;
+                        nm.yrP2o5 = nutrientInputs.P2O5_FirstYear;
+                        nm.yrK2o = nutrientInputs.K2O_FirstYear;
+                        nm.ltN = nutrientInputs.N_LongTerm;
+                        nm.ltP2o5 = nutrientInputs.P2O5_LongTerm;
+                        nm.ltK2o = nutrientInputs.K2O_LongTerm;
+
+                        _ud.UpdateFieldNutrientsManure(fld.fieldName, nm);
+                    }
+                }
+            }
         }
         public ActionResult CompostDelete(int id, string target)
         {
