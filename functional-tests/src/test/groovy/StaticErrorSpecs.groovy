5a624e7fe4992f00595356dbfdc23f35a345bd6e
GusJassal <gus.android@gmail.com>
2017-11-16 15:41:38 -0800

    Merge branch 'master' of https://github.com/GusJassal/agri-nmp
    groovey changes

--- functional-tests/src/test/groovy/StaticErrorSpecs.groovy
+++ functional-tests/src/test/groovy/StaticErrorSpecs.groovy
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import geb.spock.GebReportingSpec
 import pages.app.ValidateStaticPage
 
@@ -10,3 +11,17 @@ class StaticErrorSpecs extends GebReportingSpec {
 		assert staticDataError
     }
 }
+=======
+import geb.spock.GebReportingSpec
+import pages.app.ValidateStaticPage
+
+class StaticErrorSpecs extends GebReportingSpec {
+
+	def "Static Error Test"(){
+	when:
+		to ValidateStaticPage
+	then:
+		assert staticDataError
+    }
+}
+>>>>>>> 8fba86f6ad578b2dacd7a3c1d7d72507103e933e
