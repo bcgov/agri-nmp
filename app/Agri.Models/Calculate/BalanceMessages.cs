584c72628e3a91a00fb74a2250ffbd3f9b4d042c
CodingVelocista <anselmo.rossiello@outlook.com>
2018-11-08 13:40:08 -0800

    Added Calculate Models

---
+++ app/Agri.Models/Calculate/BalanceMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace Agri.Models.Calculate
+{
+    public class BalanceMessages
+    {
+        public string Message { get; set; }
+        public string Chemical { get; set; }
+        public string Icon { get; set; }
+        public string IconText { get; set; }
+    }
+}
---
+++ app/Agri.Models/Calculate/NOrganicMineralizations.cs
@@ -0,0 +1,8 @@
+﻿namespace Agri.Models.Calculate
+{
+    public class NOrganicMineralizations
+    {
+        public decimal OrganicN_FirstYear { get; set; }
+        public decimal OrganicN_LongTerm { get; set; }
+    }
+}
