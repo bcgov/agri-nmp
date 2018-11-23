082105b487e07e13389a2630e051d5156e39acc9
Anselmo Rossiello <anselmo.rossiello@outlook.com>
2018-11-23 09:04:02 -0800

    Fixed bug with reference

--- app/Server/src/SERVERAPI/ViewModels/ManureStorageMaterialsRequireAssigningViewModel.cs
+++ app/Server/src/SERVERAPI/ViewModels/ManureStorageMaterialsRequireAssigningViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Agri.Models.Farm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace SERVERAPI.ViewModels
     public class ManureStorageMaterialsRequireAssigningViewModel
     {
         public string Title { get; set; }
-        public List<Models.GeneratedManure> UnallocatedGeneratedManures { get; set; }
+        public List<GeneratedManure> UnallocatedGeneratedManures { get; set; }
         public string Message { get; set; }
         public string Target { get; set; }
     }
