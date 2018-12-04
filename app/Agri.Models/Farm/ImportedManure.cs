3b68bdaaf97c70b015ab011a2da6ea104a90dcc4
Anselmo Rossiello <anselmo.rossiello@outlook.com>
2018-12-04 10:13:15 -0800

    Quick change to add a parent class for Imported and Generated manure

--- app/Agri.Models/Farm/ImportedManure.cs
+++ app/Agri.Models/Farm/ImportedManure.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 
 namespace Agri.Models.Farm
 {
-    public class ImportedManure
+    public class ImportedManure : ManagedManure
     {
-        public int? id { get; set; }
         public string MaterialName { get; set; }
         public ManureMaterialType ManureType { get; set; }
         public string ManureTypeName { get; set; }
         public decimal AnnualAmount { get; set; }
         public string Units { get; set; }
         public bool IsLandAppliedBeforeStorage { get; set; }
-        public bool AssignedToStoredSystem { get; set; }
+        public override string ManureId => $"Imported{Id ?? 0}";
     }
 }
