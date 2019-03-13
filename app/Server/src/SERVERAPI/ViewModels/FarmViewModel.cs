d12f22f61e284b408f1d8494fd583f75ca543063
Anselmo Rossiello <anselmo.rossiello@outlook.com>
2019-03-13 14:34:22 -0700

    Added UI changes to FarmIncomplete to allow for selection regions and subregions.  As well updated the static.json with Regions from production along with the MigrationSeedData for SubRegions

--- app/Server/src/SERVERAPI/ViewModels/FarmViewModel.cs
+++ app/Server/src/SERVERAPI/ViewModels/FarmViewModel.cs
@@ -4,22 +4,15 @@ using Agri.Models.Configuration;
 
 namespace SERVERAPI.ViewModels
 {
-    public class FarmViewModel
+    public class FarmViewModel : FarmViewModelBase
     {
         [Display(Name = "Year")]
         [Required]
         public string year { get; set; }
         [Display(Name = "Farm Name")]
         public string farmName { get; set; }
-        public List<SelectListItem> regOptions { get; set; }
-        [Display(Name = "Region")]
-        public int? selRegOption { get; set; }
-        public List<SelectListItem> subRegionOptions { get; set; }
-        public int? selSubRegOption { get; set; }
+
         public string currYear { get; set; }
-        public bool showSubRegion { get; set; }
-        public bool multipleSubRegion { get; set; }
-        public string buttonPressed { get; set; }
         public bool HasAnimals { get; set; }
         public bool ImportsManureCompost { get; set; }
         public bool UsesFertilizer { get; set; }
