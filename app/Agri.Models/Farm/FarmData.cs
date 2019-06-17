f9de4db52747edc381899c67ed011ed6ecf22649
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-06-17 07:25:16 -0700

    Fixed issue with LastAppliedManureId causing hidden exception

--- app/Agri.Models/Farm/FarmData.cs
+++ app/Agri.Models/Farm/FarmData.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Agri.Models.Farm
 {
@@ -7,8 +9,32 @@ namespace Agri.Models.Farm
         public FarmDetails farmDetails { get; set; }
         public bool unsaved { get; set; }
         public List<YearData> years { get; set; }
-        public string LastAppliedFarmManureId { get; set; }
+
+        private string lastAppliedFarmManureId;
+
+        public string LastAppliedFarmManureId
+        {
+            get
+            {
+                lastAppliedFarmManureId = VerifyAppliedFarmManureId(lastAppliedFarmManureId);
+                return lastAppliedFarmManureId;
+            }
+            set
+            {
+                lastAppliedFarmManureId = value;
+            }
+        }
+
         public int? NMPReleaseVersion { get; set; }
+
+        private string VerifyAppliedFarmManureId(string appliedFarmManureId)
+        {
+            var yearData = years.FirstOrDefault(y => y.year == farmDetails.year);
+            if (yearData.farmManures.Any(c => c.id == Convert.ToInt32(appliedFarmManureId)))
+            {
+                return appliedFarmManureId;
+            }
+            return null;
+        }
     }
 }
-
