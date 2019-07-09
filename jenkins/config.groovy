c8490beb5b2158a3216a768cb0831df0b54f646a
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2019-07-09 09:54:46 -0700

    Update config.groovy

--- jenkins/config.groovy
+++ jenkins/config.groovy
@@ -23,7 +23,7 @@ app {
     git {
         workDir = ['git', 'rev-parse', '--show-toplevel'].execute().text.trim()
         uri = ['git', 'config', '--get', 'remote.origin.url'].execute().text.trim()
-        ref = ['bash','-c', 'git config branch.`git name-rev --name-only HEAD`.merge'].execute().text.trim()
+        ref = "refs/pull/${opt.'pr'}/head"
         commit = ['git', 'rev-parse', 'HEAD'].execute().text.trim()
     }
 
