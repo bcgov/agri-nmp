c47dbff336d35f92840c8a1729499eea407707d7
Rajvir Bains <rajvir.bains@quartech.com>
2019-11-16 17:01:28 -0800

    agri nmp new bcdk pipeline changes

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -5,9 +5,9 @@ const version = '1.0.0'
 const name = 'nmp'
 
 const phases = {
-  build: {namespace:'agri-nmp-tools', name: `${name}`, phase: 'build', changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-build-${changeId}`, version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`, nmpreplicas:'1', pdfreplicas:'1' , host:'${name}-pr-${changeId}-agri-nmp-tools.pathfinder.gov.bc.ca'},
-    dev: {namespace:'agri-nmp-dev'  , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`  , nmpreplicas:'1', pdfreplicas:'1' , host:'${name}-pr-${changeId}-agri-nmp-dev.pathfinder.gov.bc.ca'},
-   test: {namespace:'agri-nmp-test' , name: `${name}`, phase: 'test' , changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-test`             , version:`${version}`            , tag:`test-${version}`             , nmpreplicas:'2', pdfreplicas:'2' , host:'${name}-test-agri-nmp-test.pathfinder.gov.bc.ca'},
+  build: {namespace:'agri-nmp-tools', name: `${name}`, phase: 'build', changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-build-${changeId}`, version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`, nmpreplicas:'1', pdfreplicas:'1' , host:`${name}-pr-${changeId}-agri-nmp-tools.pathfinder.gov.bc.ca`},
+    dev: {namespace:'agri-nmp-dev'  , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`  , nmpreplicas:'1', pdfreplicas:'1' , host:`${name}-pr-${changeId}-agri-nmp-dev.pathfinder.gov.bc.ca`},
+   test: {namespace:'agri-nmp-test' , name: `${name}`, phase: 'test' , changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-test`             , version:`${version}`            , tag:`test-${version}`             , nmpreplicas:'2', pdfreplicas:'2' , host:`${name}-test-agri-nmp-test.pathfinder.gov.bc.ca`},
    prod: {namespace:'agri-nmp-prod' , name: `${name}`, phase: 'prod' , changeId:changeId, suffix: `-pr-${changeId}`, instance: `${name}-prod`             , version:`${version}`            , tag:`prod-${version}`             , nmpreplicas:'3', pdfreplicas:'3' , host:'nmp.apps.nrs.gov.bc.ca'}
 };
 
