e4f0a5af652146da8cc524c9e5d734c6560196d8
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-12-30 20:54:02 -0800

    Stub pages for Nutrients

---
+++ app/Server/src/SERVERAPI/Pages/RanchNutrients/Delete.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SERVERAPI.Pages.RanchNutrients
+{
+    public class Delete : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
