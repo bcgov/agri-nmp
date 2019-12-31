b70c4139dfa7bce20747361303e3d5f58e319db7
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2019-12-31 05:35:17 -0800

    Initial Code for CreateEdit

--- app/Server/src/SERVERAPI/Pages/RanchNutrients/CreateEdit.cshtml.cs
+++ app/Server/src/SERVERAPI/Pages/RanchNutrients/CreateEdit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,37 @@ namespace SERVERAPI.Pages.RanchNutrients
 {
     public class CreateEdit : BasePageModel
     {
-        public void OnGet()
+        private readonly IMediator _mediator;
+
+        [BindProperty]
+        public Command Data { get; set; }
+
+        [BindProperty]
+        public bool IsModal { get; set; }
+
+        public CreateEdit(IMediator mediator)
         {
+            _mediator = mediator;
+        }
+
+        public async Task OnGetCreateAsync()
+        {
+            Data = await _mediator.Send(new Query());
+        }
+
+        public async Task OnGetEditAsync(Query query)
+        {
+            Data = await _mediator.Send(query);
+        }
+
+        public class Query : IRequest<Command>
+        {
+            public int? Id { get; set; }
+        }
+
+        public class Command : IRequest<Unit>
+        {
+            public int? Id { get; set; }
         }
     }
 }
