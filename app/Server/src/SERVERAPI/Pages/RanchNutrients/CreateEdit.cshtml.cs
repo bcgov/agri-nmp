408142cdb37f4350ad32579b3e3d81490d6792e5
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-01-05 22:29:56 -0800

    Added initial handler fo query

--- app/Server/src/SERVERAPI/Pages/RanchNutrients/CreateEdit.cshtml.cs
+++ app/Server/src/SERVERAPI/Pages/RanchNutrients/CreateEdit.cshtml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Agri.Data;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SERVERAPI.Models.Impl;
 
 namespace SERVERAPI.Pages.RanchNutrients
 {
@@ -41,6 +46,38 @@ namespace SERVERAPI.Pages.RanchNutrients
         public class Command : IRequest<Unit>
         {
             public int? Id { get; set; }
+            public SelectList RanchManures { get; set; }
+            public SelectList BeefNutrientAnalysisOptions { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Command>
+        {
+            private readonly UserData _ud;
+            private readonly IMapper _mapper;
+            private readonly IAgriConfigurationRepository _sd;
+
+            public Handler(UserData ud, IMapper mapper, IAgriConfigurationRepository sd)
+            {
+                _ud = ud;
+                _mapper = mapper;
+                _sd = sd;
+            }
+
+            public Task<Command> Handle(Query request, CancellationToken cancellationToken)
+            {
+                //var ranchManures = _ud.
+                var beefManuresNutrients = _sd.GetManures()
+                    .Where(m => m.ManureClass.Contains("Beef - feedlot"))
+                    .Select(m => new { Id = m.Id, Name = m.Name })
+                    .ToList();
+
+                //command.CattleSubTypeOptions = new SelectList(subTypeOptions, "Id", "Value");
+                var command = new Command
+                {
+                    BeefNutrientAnalysisOptions = new SelectList(beefManuresNutrients, "Id", "Name")
+                };
+                return Task.FromResult(command);
+            }
         }
     }
 }
