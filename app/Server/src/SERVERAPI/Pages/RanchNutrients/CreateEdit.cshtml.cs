be28e9df7d990ae28a53729cb1e224da57402525
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-01-07 08:45:00 -0800

    Added fields for nutrients to code behind

--- app/Server/src/SERVERAPI/Pages/RanchNutrients/CreateEdit.cshtml.cs
+++ app/Server/src/SERVERAPI/Pages/RanchNutrients/CreateEdit.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Agri.Data;
+using Agri.Models;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +27,7 @@ namespace SERVERAPI.Pages.RanchNutrients
 
         public CreateEdit(IMediator mediator)
         {
+            Title = "Nutrient Analysis";
             _mediator = mediator;
         }
 
@@ -38,6 +41,26 @@ namespace SERVERAPI.Pages.RanchNutrients
             Data = await _mediator.Send(query);
         }
 
+        protected async Task<IActionResult> OnPostCreateAsync()
+        {
+            return await HandlePostBack();
+        }
+
+        protected async Task<IActionResult> OnPostEditAsync()
+        {
+            return await HandlePostBack();
+        }
+
+        private async Task<IActionResult> HandlePostBack()
+        {
+            if (Data.PostedElementEvent == ElementEvent.UseCustomAnalysis)
+            {
+                Data.UseBookValue = false;
+            }
+            Data = await _mediator.Send(new Query());
+            return Page();
+        }
+
         public class Query : IRequest<Command>
         {
             public int? Id { get; set; }
@@ -47,7 +70,59 @@ namespace SERVERAPI.Pages.RanchNutrients
         {
             public int? Id { get; set; }
             public SelectList RanchManures { get; set; }
+            public int SelectedNutrientAnalysis { get; set; }
             public SelectList BeefNutrientAnalysisOptions { get; set; }
+
+            public string Action { get; set; }
+            public string SourceOfMaterialName { get; set; }
+
+            [Display(Name = "Material Type")]
+            public string ManureName { get; set; }
+
+            public ManureMaterialType MaterialType { get; set; }
+
+            [Display(Name = "Moisture (%)")]
+            public string Moisture { get; set; }
+
+            [Display(Name = "N (%)")]
+            public string Nitrogen { get; set; }
+
+            [Display(Name = "NH<sub>4</sub>-N (ppm)")]
+            public string Ammonia { get; set; }
+
+            [Display(Name = "P (%)")]
+            public string Phosphorous { get; set; }
+
+            [Display(Name = "K (%)")]
+            public string Potassium { get; set; }
+
+            [Display(Name = "NO<sub>3</sub>-N (ppm)")]
+            public string Nitrate { get; set; }
+
+            public bool UseBookValue { get; set; } = true;
+            public bool Compost { get; set; }
+            public string MoistureBook { get; set; }
+            public string NitrogenBook { get; set; }
+            public string AmmoniaBook { get; set; }
+            public string NitrateBook { get; set; }
+            public string PhosphorousBook { get; set; }
+            public string PotassiumBook { get; set; }
+            public bool ShowNitrate { get; set; }
+            public NutrientAnalysisTypes StoredImported { get; set; }
+            public string ExplainNutrientAnalysisMoisture { get; set; }
+            public string ExplainNutrientAnalysisNitrogen { get; set; }
+            public string ExplainNutrientAnlalysisAmmonia { get; set; }
+            public string ExplainNutrientAnlalysisPhosphorous { get; set; }
+            public string ExplainNutrientAnlalysisPotassium { get; set; }
+            public bool IsLegacyNMPReleaseVersion { get; set; }
+            public int? LegacyNMPReleaseVersionManureId { get; set; }
+            public ElementEvent PostedElementEvent { get; set; }
+        }
+
+        public enum ElementEvent
+        {
+            None,
+            UseCustomAnalysis
         }
 
         public class Handler : IRequestHandler<Query, Command>
@@ -67,7 +142,7 @@ namespace SERVERAPI.Pages.RanchNutrients
             {
                 //var ranchManures = _ud.
                 var beefManuresNutrients = _sd.GetManures()
-                    .Where(m => m.ManureClass.Contains("Beef - feedlot"))
+                    .Where(m => m.ManureClass.Contains("Beef"))
                     .Select(m => new { Id = m.Id, Name = m.Name })
                     .ToList();
 
--- app/Server/src/SERVERAPI/Pages/RanchNutrients/Index.cshtml.cs
+++ app/Server/src/SERVERAPI/Pages/RanchNutrients/Index.cshtml.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SERVERAPI.Models.Impl;
 
 namespace SERVERAPI.Pages.RanchNutrients
 {
     public class Index : BasePageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Model Data { get; set; }
+
+        public async Task OnGetAsync()
         {
+        }
+
+        public class Query : IRequest<Model>
+        {
+        }
+
+        public class Model
+        {
+        }
+
+        public class Handler : IRequestHandler<Query, Model>
+        {
+            private readonly UserData _ud;
+            private readonly IMapper _mapper;
+
+            public Handler(UserData ud, IMapper mapper)
+            {
+                _ud = ud;
+                _mapper = mapper;
+            }
+
+            public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
