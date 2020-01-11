8080eed2f2c3eb066c7f6bff7f4a0e994937bad7
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-01-11 12:54:21 -0800

    Bug fix from refactor changes

--- app/Agri.Models/Farm/Field.cs
+++ app/Agri.Models/Farm/Field.cs
@@ -13,7 +13,7 @@ namespace Agri.Models.Farm
         public string fieldName { get; set; }
         public decimal area { get; set; }
         public string comment { get; set; }
-        public Nutrients nutrients { get; set; }
+        public Nutrients nutrients { get; set; } = new Nutrients();
         public List<FieldCrop> crops { get; set; }
         public SoilTest soilTest { get; set; }
         public string prevYearManureApplicationFrequency { get; set; }
