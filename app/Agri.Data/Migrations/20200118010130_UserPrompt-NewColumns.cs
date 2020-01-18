be2150ea93f86d5d274c34c519281647f510e05e
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-01-17 17:29:00 -0800

    Added migration

---
+++ app/Agri.Data/Migrations/20200118010130_UserPrompt-NewColumns.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agri.Data.Migrations
+{
+    public partial class UserPromptNewColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserPromptPage",
+                table: "UserPrompts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserJourney",
+                table: "UserPrompts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserJourney",
+                table: "UserPrompts");
+
+            migrationBuilder.DropColumn(
+                name: "UserPromptPage",
+                table: "UserPrompts");
+        }
+    }
+}
