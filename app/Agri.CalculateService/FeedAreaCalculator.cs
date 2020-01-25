8293b5dfe1d0675adf599ebcda89841c78919250
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-01-24 21:25:26 -0800

    Stub class calculator

---
+++ app/Agri.CalculateService/FeedAreaCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agri.CalculateService
+{
+    public interface IFeedAreaCalculator
+    {
+        decimal K20AgronomicBalance();
+
+        decimal NAgronomicBalance();
+
+        decimal P205AgronomicBalance();
+    }
+
+    public class FeedAreaCalculator : IFeedAreaCalculator
+    {
+        public decimal NAgronomicBalance()
+        {
+            var result = 0M;
+
+            return result;
+        }
+
+        public decimal P205AgronomicBalance()
+        {
+            var result = 0M;
+
+            return result;
+        }
+
+        public decimal K20AgronomicBalance()
+        {
+            var result = 0M;
+
+            return result;
+        }
+    }
+}
