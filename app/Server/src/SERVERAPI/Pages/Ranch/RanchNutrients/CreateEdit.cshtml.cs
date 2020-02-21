c8ac689c2bb79e45de2bc39eb337ef4b9742148b
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-02-21 14:53:13 -0800

    Added preselecting of all materials

--- app/Server/src/SERVERAPI/Pages/Ranch/RanchNutrients/CreateEdit.cshtml.cs
+++ app/Server/src/SERVERAPI/Pages/Ranch/RanchNutrients/CreateEdit.cshtml.cs
@@ -55,8 +55,14 @@ namespace SERVERAPI.Pages.Ranch.RanchNutrients
 
         public async Task<IActionResult> OnPostAsync()
         {
+            Data.ExcludedSourceOfMaterialIds.Clear();
             Data.IncludedSourceOfMaterialIds.Clear();
 
+            if (Data.RanchManures.Any(rm => !rm.Selected))
+            {
+                Data.ExcludedSourceOfMaterialIds
+                    .AddRange(Data.RanchManures.Where(rm => !rm.Selected).Select(rm => rm.ManureId).ToList());
+            }
             if (Data.RanchManures.Any(rm => rm.Selected))
             {
                 Data.IncludedSourceOfMaterialIds
@@ -153,6 +159,7 @@ namespace SERVERAPI.Pages.Ranch.RanchNutrients
             public List<RanchManure> RanchManures { get; set; }
             public int SelectedNutrientAnalysis { get; set; }
             public SelectList BeefNutrientAnalysisOptions { get; set; }
+            public List<string> ExcludedSourceOfMaterialIds { get; set; } = new List<string>();
             public List<string> IncludedSourceOfMaterialIds { get; set; } = new List<string>();
 
             [Display(Name = "Material Type")]
@@ -199,9 +206,7 @@ namespace SERVERAPI.Pages.Ranch.RanchNutrients
             {
                 public string ManureId { get; set; }
                 public string ManureName { get; set; }
-
-                [IgnoreMap]
-                public bool Selected { get; set; }
+                public bool Selected { get; set; } = true;
             }
 
             public class ManureNutrientBookValues
@@ -243,7 +248,8 @@ namespace SERVERAPI.Pages.Ranch.RanchNutrients
                     .ForMember(m => m.UseCustomAnalysis, opts => opts.MapFrom(s => s.Customized))
                     .ReverseMap();
                 CreateMap<ManagedManure, Command.RanchManure>()
-                    .ForMember(m => m.ManureName, opts => opts.MapFrom(s => s.ManagedManureName));
+                    .ForMember(m => m.ManureName, opts => opts.MapFrom(s => s.ManagedManureName))
+                    .BeforeMap((s, d) => d.Selected = true);
                 CreateMap<Manure, Command.ManureNutrientBookValues>();
             }
         }
@@ -382,14 +388,14 @@ namespace SERVERAPI.Pages.Ranch.RanchNutrients
 
                 var manures = _ud.GetAllManagedManures()
                     .Where(mm => !mm.AssignedWithNutrientAnalysis ||
-                        request.PopulatedData.IncludedSourceOfMaterialIds
+                        request.PopulatedData.ExcludedSourceOfMaterialIds
                             .Any(im => im.Equals(mm.ManureId, StringComparison.OrdinalIgnoreCase)))
                     .ToList();
 
                 command.RanchManures = _mapper.Map<List<Command.RanchManure>>(manures);
                 foreach (var manure in command.RanchManures)
                 {
-                    manure.Selected = request.PopulatedData.IncludedSourceOfMaterialIds
+                    manure.Selected = !request.PopulatedData.ExcludedSourceOfMaterialIds
                         .Any(m => m.Equals(manure.ManureId));
                 }
 
