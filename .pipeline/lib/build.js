2186ca2fb37e2ed238e98cf0c5400535266cadca
Anselmo Rossiello <Anselmo.Rossiello@quartech.com>
2020-03-05 09:36:01 -0800

    Fixed build.js reference to old dotnet-21.bc.json

--- .pipeline/lib/build.js
+++ .pipeline/lib/build.js
@@ -1,51 +1,51 @@
 'use strict';
-const {OpenShiftClientX} = require('pipeline-cli')
+const { OpenShiftClientX } = require('pipeline-cli')
 const path = require('path');
 
-module.exports = (settings)=>{
-  const phases = settings.phases
-  const options = settings.options
-  const phase='build'
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
-  const objects = []
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../OpenShift'))
+module.exports = (settings) => {
+    const phases = settings.phases
+    const options = settings.options
+    const phase = 'build'
+    const oc = new OpenShiftClientX(Object.assign({ 'namespace': phases.build.namespace }, options));
+    const objects = []
+    const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../OpenShift'))
 
-  // For weasyprint we  won't create containers for each PR. Just one for the whole DEV.
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/weasyprint-bc.json`, {
-    'param':{
-    'NAME': phases[phase].name,
-    'SUFFIX': phases[phase].suffix,
-    'VERSION': phases[phase].tag,
-    }
-  }));
+    // For weasyprint we  won't create containers for each PR. Just one for the whole DEV.
+    objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/weasyprint-bc.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+        }
+    }));
 
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/dotnet-21.bc.json`, {
-    'param':{
-    'NAME': phases[phase].name,
-    'SUFFIX': phases[phase].suffix,
-    'VERSION': phases[phase].tag,
-    }
-  }));
+    objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/dotnet.bc.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+        }
+    }));
 
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/dotnet-node.bc.json`, {
-    'param':{
-      'NAME': phases[phase].name,
-      'SUFFIX': phases[phase].suffix,
-      'VERSION': phases[phase].tag,
-      'SOURCE_REPOSITORY_URL': oc.git.http_url,
-      'GIT_REF': oc.git.ref
-    }
-  }));
+    objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/dotnet-node.bc.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+            'SOURCE_REPOSITORY_URL': oc.git.http_url,
+            'GIT_REF': oc.git.ref
+        }
+    }));
 
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/nmp.bc.json`, {
-    'param':{
-      'NAME': phases[phase].name,
-      'SUFFIX': phases[phase].suffix,
-      'VERSION': phases[phase].tag,
-      'SOURCE_REPOSITORY_URL': oc.git.http_url,
-      'GIT_REF': oc.git.ref
-    }
-  }));
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
-  oc.applyAndBuild(objects)
+    objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/nmp.bc.json`, {
+        'param': {
+            'NAME': phases[phase].name,
+            'SUFFIX': phases[phase].suffix,
+            'VERSION': phases[phase].tag,
+            'SOURCE_REPOSITORY_URL': oc.git.http_url,
+            'GIT_REF': oc.git.ref
+        }
+    }));
+    oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
+    oc.applyAndBuild(objects)
 }
