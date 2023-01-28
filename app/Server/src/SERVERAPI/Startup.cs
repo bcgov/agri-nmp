f2fc7c77abca0a1f6b6b9bda03345b93122cdf4f
Dmitri Korin <dmitri.korin@gov.bc.ca>
2023-01-27 16:44:53 -0800

    Upgrade .NET 7.0 and affectd NuGet packages

--- app/Server/src/SERVERAPI/Startup.cs
+++ app/Server/src/SERVERAPI/Startup.cs
@@ -166,6 +166,8 @@ namespace SERVERAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
             var cultureInfo = new CultureInfo("en-US");
             cultureInfo.NumberFormat.CurrencySymbol = "$";
 
