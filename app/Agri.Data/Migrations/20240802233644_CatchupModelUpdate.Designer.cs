db0b97d21c05c709c4399ae12fff46604de52212
acatchpole <adamantiumite@gmail.com>
2024-08-02 16:43:12 -0700

    added migartion to catch up on missed changes from model updates done without migrations

--- app/Agri.Data/Migrations/20240802233644_CatchupModelUpdate.Designer.cs
+++ app/Agri.Data/Migrations/20240802233644_CatchupModelUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Agri.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Agri.Data.Migrations
 {
     [DbContext(typeof(AgriConfigurationContext))]
-    partial class AgriConfigurationContextModelSnapshot : ModelSnapshot
+    [Migration("20240802233644_CatchupModelUpdate")]
+    partial class CatchupModelUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
