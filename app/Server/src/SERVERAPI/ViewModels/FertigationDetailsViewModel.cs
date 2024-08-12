2d1c3b130434dfc2fb7d87c7fa6f0515ee4498f3
Dallas Richmond <dallas.richmond@gov.bc.ca>
2024-08-12 15:16:35 -0700

    Added fertigation button and empty modal

---
+++ app/Server/src/SERVERAPI/ViewModels/FertigationDetailsViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using Agri.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Agri.Models.Configuration;
+
+namespace SERVERAPI.ViewModels
+{
+	public class FertigationDetailsViewModel
+	{
+        public int? id { get; set; }
+        public string title { get; set; }
+
+    }
+}
